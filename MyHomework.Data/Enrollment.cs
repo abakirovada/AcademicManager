c301d8f492ff4635a17808d286258f1480af611d
Dinara Abakirova <abakirovada@gmail.com>
2021-10-07 14:58:25 -0400

    Added Class Controller and service

---
+++ MyHomework.Data/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework.Data
+{
+    public class Enrollment
+    {
+        [Key]
+        public int EnrollmentId { get; set; }
+
+        public int ClassId { get; set; }
+        public int StudentId { get; set; }
+    }
+}
